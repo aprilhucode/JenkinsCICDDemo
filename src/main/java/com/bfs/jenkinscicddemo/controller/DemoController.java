cc338bdc2df01fbb1369a9696064bee4738c3bf6
Pinxuan Huang <phuang@beaconfireinc.com>
2023-01-05 21:19:34 -0500

    Update DemoController.java

--- src/main/java/com/bfs/jenkinscicddemo/controller/DemoController.java
+++ src/main/java/com/bfs/jenkinscicddemo/controller/DemoController.java
@@ -2,7 +2,9 @@ package com.bfs.jenkinscicddemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DemoController {
 
     @GetMapping({"/", "/demo"})
